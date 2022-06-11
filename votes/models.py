6d0ee0fb4a936453e2ff19d23cd3456127909737
Dean Kago <martokhago@gmail.com>
2022-06-12 00:20:12 +0300

    created rating class in models

--- votes/models.py
+++ votes/models.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Profile(models.Model):
+  name = models.TextField(null=True, blank=True)
   prof_image = models.ImageField(null=True, blank=True)
   user_bio = models.TextField(blank=True, null=True)
   projects = models.CharField(null=True, blank=True, max_length=180)
@@ -19,3 +20,14 @@ class Project(models.Model):
   imagee = models.ImageField(null=True, blank=True)
   description = models.TextField(null=True, blank=True)
   link = models.URLField(null=True, blank=True)
+
+  profile = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+
+
+
+class rating(models.Model):
+  design = models.IntegerField(null=True, blank=True)
+  usability = models.IntegerField(null=True, blank=True)
+  content = models.IntegerField(null=True, blank=True)
+
+  project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
