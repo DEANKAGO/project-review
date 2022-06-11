9027fa1e3e550bef375bc72f73aed0337fcc6d33
Dean Kago <martokhago@gmail.com>
2022-06-11 23:25:12 +0300

    created profile class in models

--- reviews/settings.py
+++ reviews/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'votes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'reviews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'review',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- votes/models.py
+++ votes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+
+
 
 # Create your models here.
+
+class Profile(models.Model):
+  prof_image = models.ImageField(null=True, blank=True)
+  user_bio = models.TextField(blank=True, null=True)
+  projects = models.CharField(null=True, blank=True, max_length=180)
+  contact = models.CharField(null=True, blank=True, max_length=180)
+
+
+
