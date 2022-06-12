694883a6da517098d3af31f4a2460cf6f3c4e12e
Dean Kago <martokhago@gmail.com>
2022-06-12 16:39:08 +0300

    added search profile function

--- votes/views.py
+++ votes/views.py
@@ -7,5 +7,18 @@ def homepage(request):
   return render(request, 'main/index.html')
 
 
-def rating(request):
-  return render(request, 'main/rating.html')
+def rating(request, slug):
+  profile = Profile.objects.get(slug=slug)
+  projects = Project.objects.filter(profile=profile).order_by('-date_created')
+  return render(request, 'main/rating.html', locals())
+
+
+def search_profile(request):
+  if 'profile' in request.GET and request.GET['profile']:
+    searched_profile = request.GET['profile']
+    profiles = Profile.searching_profile(searched_profile)
+    term = f"{searched_profile}"
+    return render(request, 'main/search.html', locals())
+  else:
+    term = "kindly input a profile name"
+    return render(request, 'main/search.html', locals())
