0e778dad86db43c62c19b8ad74c9a4bef8aeb4e0
Dean Kago <martokhago@gmail.com>
2022-06-12 12:57:41 +0300

    added slug in rating model

--- votes/models.py
+++ votes/models.py
@@ -70,9 +70,31 @@ class Project(models.Model):
 
 
 
-class rating(models.Model):
+class Rating(models.Model):
   design = models.IntegerField(null=True, blank=True)
   usability = models.IntegerField(null=True, blank=True)
   content = models.IntegerField(null=True, blank=True)
 
   project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
+
+  uniqueId = models.CharField(null=True, blank=True, max_length=100)
+  slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+  date_created = models.DateTimeField(blank=True, null=True)
+  last_updated = models.DateTimeField(blank=True, null=True)
+
+  def __str__(self):
+    return '{} {}'.format(self.project.title, self.uniqueId)
+
+  def get_absolute_url(self):
+    return reverse('rating-detail', kwargs={'slug': self.slug})
+
+  def save(self, *args, **kwargs):
+    if self.date_created is None:
+      self.date_created = timezone.localtime(timezone.now())
+    if self.uniqueId is None:
+      self.uniqueId = str(uuid4()).split('-')[4]
+      self.slug = slugify('{} {}'.format(self.project.title, self.uniqueId))
+
+    self.slug = slugify('{} {}'.format(self.project.title, self.uniqueId))
+    self.last_updated = timezone.localtime(timezone.now())
+    super(Rating, self).save(*args, **kwargs)
