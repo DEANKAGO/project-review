09c9de9d68f8de01accf1c82ba08f7ac424d423f
Dean Kago <martokhago@gmail.com>
2022-06-13 23:58:40 +0300

    added logout url path

--- votes/forms.py
+++ votes/forms.py
@@ -4,7 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import * 
 
 
+class RegistrationForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
 
+    def save(self, commit=True):
+        user=super().save(commit=False)
+        user.email=self.cleaned_data['email']
+        if commit:
+
+            user.save()
+        return user
+
+
+        
 class projectForm(forms.ModelForm):  
   class Meta:
     model = Projects
