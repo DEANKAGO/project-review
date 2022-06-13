bf3362f37ec475190f3838e0e5a7eb0e86c202db
Dean Kago <martokhago@gmail.com>
2022-06-13 23:47:07 +0300

    added register user function

--- votes/views.py
+++ votes/views.py
@@ -14,6 +14,28 @@ from rest_framework.views import APIView
 
 # Create your views here.
 
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        procForm=profileForm(request.POST, request.FILES)
+        if form.is_valid() and procForm.is_valid():
+            username=form.cleaned_data.get('username')
+            user=form.save()
+            profile=procForm.save(commit=False)
+            profile.user=user
+            profile.save()
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+        prof=profileForm()
+    params={
+        'form':form,
+        'profForm': prof
+    }
+    return render(request, 'users/register.html', params)
+
+
+
 def index(request):
   projects = Projects.objects.all()
   return render(request, 'main/index.html', {'projects': projects})
